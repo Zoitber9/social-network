370b9da7d481f5bae49a12ca6dc59da77febdd27
Andrei <zoitber9@mail.ru>
2023-03-30 01:16:49 +0300

    feat: 54 lesson (жизненный цикл, life cycle methods, componentDidMount)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,15 +12,13 @@ type UsersPropsType = {
 }
 
 class Users extends React.Component<UsersPropsType, {}> {
-    constructor(props: any) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                     this.props.setUsers(response.data.items)
                 }
             )
     }
-
     render() {
         return <div>
             {
